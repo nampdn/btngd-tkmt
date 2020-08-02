5234e5254ff62c1885a59f9546ef60111c9b4954
nampdn <nampdn@protonmail.com>
2020-08-02 11:48:34 +0700

    feat: 🎸 style

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+import "./styles/tailwind-pre-build.css";
+import Navbar from "./components/Navbar";
+import LivePortal from "./components/LivePortal";
+import WorkContainer from "./components/WorkContainer";
+// import HomePage from "./components/Homepage";
+import FormPage from "./components/FormPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div style={{ backgroundColor: "#F8F8F8" }}>
+        {/* <Navbar /> */}
+        <Switch>
+          <Route path="/" exact component={FormPage} />
+          <Route path="/liveportal" component={LivePortal} />
+          <Route path="/workcontainer" component={WorkContainer} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
